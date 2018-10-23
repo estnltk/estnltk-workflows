18d08c635438aceff247bc4e835c22d472d06ff0
pault <pault@ut.ee>
2018-10-23 20:56:11 +0300

    updated arg parsing and logging in export_to_texta

--- estnltk_workflows/postgres_collections/data_export/export_to_texta.py
+++ estnltk_workflows/postgres_collections/data_export/export_to_texta.py
@@ -6,11 +6,13 @@ parser = argparse.ArgumentParser(description='Export Estnltk postgres collection
                                         '$export_to_texta @conf',
                                  fromfile_prefix_chars='@')
 
+parser.add_argument('--database', dest='database', action='store',
+                    help='name of the PostgreSQL database (default: first in the pgpass file)')
 parser.add_argument('--input-layers', dest='input_layers', action='store', nargs='+', metavar='LAYER',
                     default=[],
                     help='names of the detached layers in the collection that need to be selected '
                          'but are not listed in the fact-mapping file')
-parser.add_argument('--fact-mapping', dest='fact_mapping', action='store', default=None,
+parser.add_argument('--fact-mapping', dest='fact_mapping', action='store', default=None, nargs='?',
                     help='name of the csv file that contains fact mapping instructions (default: None)')
 parser.add_argument('--sessionpass', dest='sessionpass', action='store',
                     default=None,
@@ -20,8 +22,7 @@ parser.add_argument('--pgpass', dest='pgpass', action='store',
                     help='name of the PostgreSQL password file (default: ~/.pgpass). '
                          'File format: hostname:port:database:username:password')
 parser.add_argument('--textaurl', dest='textaurl', action='store',
-                    default='http://localhost:8000',
-                    help='TEXTA server URL (default: http://localhost:8000)')
+                    help='TEXTA server URL with port, for example: http://localhost:8000')
 parser.add_argument('--textapass', dest='textapass', action='store',
                     default='~/.textapass',
                     help='name of the file that contains TEXTA access data (default: ~/.textapass). '
@@ -32,17 +33,15 @@ parser.add_argument('--textaindex', dest='textaindex', action='store',
 parser.add_argument('--textamapping', dest='textamapping', action='store',
                     default=None,
                     help='name of the TEXTA mapping (default: collection name)')
-parser.add_argument('--schema', dest='schema', action='store',
-                    default='grammarextractor',
-                    help='name of the collection schema (default: grammarextractor)')
+parser.add_argument('--schema', dest='schema', action='store', nargs='?',
+                    help='name of the collection schema')
 parser.add_argument('--collection', dest='collection', action='store',
                     default='collection',
                     help='name of the collection (default: collection)')
-#parser.add_argument('--role', dest='role', action='store',
-#                    default='egcut_epi_grammarextractor_create',
-#                    help='collection owner (default: egcut_epi_grammarextractor_create)')
-#parser.add_argument('--mode', dest='mode', action='store', choices=['overwrite', 'append'],
-#                    help='required if the TEXTA object already exists')
+parser.add_argument('--role', dest='role', action='store', nargs='?',
+                    help='collection owner')
+parser.add_argument('--mode', dest='mode', action='store', choices=['overwrite', 'append'],
+                    help='required if the TEXTA object already exists')
 parser.add_argument('--logging', dest='logging', action='store', default='INFO',
                     choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
                     help='logging level (default: INFO)')
@@ -54,11 +53,16 @@ from estnltk.converters import TextaExporter
 from estnltk import logger
 
 
-storage = PostgresStorage(pgpass_file=args.pgpass,
-                          schema=args.schema)
+logger.info('start script')
+
+storage = PostgresStorage(dbname=args.database,
+                          user=None,
+                          pgpass_file=args.pgpass,
+                          schema=args.schema,
+                          role=args.role
+                          )
 
 collection = storage.get_collection(args.collection)
-
 
 exporter = TextaExporter(index=args.textaindex or args.schema,
                          doc_type=args.textamapping or args.collection,
@@ -69,6 +73,12 @@ exporter = TextaExporter(index=args.textaindex or args.schema,
 
 layers = args.input_layers + exporter.fact_layers
 
-for collection_id, text in collection.select(layers=layers):
-    response = exporter.export(text, meta={'collection_id': collection_id})
-    logger.info('{}, {}'.format(collection_id, response.text))
+try:
+    for collection_id, text in collection.select(layers=layers, progressbar='unicode'):
+        response = exporter.export(text, meta={'collection_id': collection_id})
+
+    logger.info('end script')
+except Exception as e:
+    logger.error(e)
+finally:
+    storage.close()
